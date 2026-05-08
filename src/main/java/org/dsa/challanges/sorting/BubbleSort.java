98f29aa5daebef069ff9f33092c42e31f79cad22
lucasoliveira04 <camposdlucasoli@gmail.com>
2026-05-07 22:14:13 -0300

    BubbleSort.java

---
+++ src/main/java/org/dsa/challanges/sorting/BubbleSort.java
@@ -0,0 +1,21 @@
+package org.dsa.challanges.sorting;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {2, 10, 4, 54, 3, 5, 10};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
