d53ec802cb2cb052469e6aa5ec9ee3c87ce0f089
lucasoliveira04 <camposdlucasoli@gmail.com>
2026-05-09 19:51:37 -0300

    Filas.java

---
+++ src/main/java/org/dsa/filas/Filas.java
@@ -0,0 +1,62 @@
+package org.dsa.filas;
+
+import java.util.EmptyStackException;
+
+public class Filas {
+    int[] data;
+    int head;
+    int tail;
+    private static final int MAX_SIZE = 100;
+    Filas() {
+        data = new int[MAX_SIZE];
+        head = 0;
+        tail = 0;
+    }
+
+    Filas(int size) {
+        data = new int[size];
+        head = 0;
+        tail = 0;
+    }
+
+    boolean isEmpty() {
+        return head == tail;
+    }
+    boolean isFull() {
+        return tail == MAX_SIZE;
+    }
+
+    int size() {
+        return tail - head;
+    }
+
+    void push(int value) {
+        data[tail] = value;
+        tail = (tail + 1) % MAX_SIZE;
+    }
+
+    int pop() {
+        if (isEmpty()) throw new EmptyStackException();
+        return data[head++];
+    }
+
+    void clear() {
+        head = 0;
+        tail = 0;
+    }
+    void print() {
+        for (int i = head; i != tail; i = (i + 1) % MAX_SIZE) {
+            System.out.print(data[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Filas filas = new Filas(MAX_SIZE);
+
+        filas.push(1);
+        filas.push(2);
+
+        filas.print();
+    }
+}
