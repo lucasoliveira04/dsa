ae0102eaf549df79d46df60f1cccf74fd3679bf4
lucasoliveira04 <camposdlucasoli@gmail.com>
2026-05-14 17:09:32 -0300

    StringToInteger.java

---
+++ src/main/java/org/dsa/challanges/Strings/StringToInteger.java
@@ -0,0 +1,32 @@
+package org.dsa.challanges.Strings;
+
+public class StringToInteger {
+
+    public static void main(String[] args) {
+        int i = 0;
+        int sign = 1;
+        int resultado = 0;
+
+        String s = "   -42";
+
+        while (i < s.length() && s.charAt(i) == ' ') i++;
+
+        if (i < s.length() && (s.charAt(i) == "+" || s.charAt(i) == "-")) {
+            sign = s.charAt(i) == '-' ? -1 : 1;
+            i++;
+        }
+
+        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+            resultado = resultado * 10 + (s.charAt(i) - '0');
+
+            if (resultado * sign > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            if (resultado * sign < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+
+            i++;
+        }
+
+        return (int) resultado * sign;
+
+
+    }
+}
