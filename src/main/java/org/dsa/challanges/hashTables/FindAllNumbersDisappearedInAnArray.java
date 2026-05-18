2405ac6fbc4845e7b872b1c276d4be75ececdbaa
lucasoliveira04 <camposdlucasoli@gmail.com>
2026-05-18 16:53:04 -0300

    FindAllNumbersDisappearedInAnArray.java

---
+++ src/main/java/org/dsa/challanges/hashTables/FindAllNumbersDisappearedInAnArray.java
@@ -0,0 +1,31 @@
+package org.dsa.challanges.hashTables;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class FindAllNumbersDisappearedInAnArray {
+    public static void main(String[] args) {
+        int[] nums = {4, 3, 2, 7, 8, 2, 3, 1};
+        List<Integer> result = findDisappearedNumbers(nums);
+        System.out.println(result);
+    }
+
+    static List<Integer> findDisappearedNumbers(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+
+        return result;
+    }
+}
