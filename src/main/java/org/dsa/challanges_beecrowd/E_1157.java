7c035c51035c40d8e5a303e3e953ad7f066ad525
Lucas Oliveira <lucasolisocialmedia@gmail.com>
2026-08-06 21:34:48 -0300

    1157

---
+++ src/main/java/org/dsa/challanges_beecrowd/E_1157.java
@@ -0,0 +1,22 @@
+package org.dsa.challanges_beecrowd;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class E_1157 {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+      Scanner sc = new Scanner(System.in);
+
+      int n = sc.nextInt();
+  
+      for (int i = 1; i <= n; i++){
+        if (n % i == 0){
+          System.out.println(i);
+        }
+      }
+ 
+    }
+ 
+}
