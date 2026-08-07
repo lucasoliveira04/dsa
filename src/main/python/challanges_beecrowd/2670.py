45696d48cc1a86f7817fa67c7360dc933352af6b
Lucas Oliveira <lucasolisocialmedia@gmail.com>
2026-08-06 21:38:18 -0300

    2670

---
+++ src/main/python/challanges_beecrowd/2670.py
@@ -0,0 +1,7 @@
+a1, a2, a3 = int(input()), int(input()), int(input())
+
+time_andar1 = 2 * a2 + 4 * a3
+time_andar2 = 2 * a1 + 2 * a3
+time_andar3 = 4 * a1 + 2 * a2
+
+print(min(time_andar1, time_andar2, time_andar3))
