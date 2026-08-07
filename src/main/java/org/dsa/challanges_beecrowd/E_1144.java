a6b09fdcbecd8704b2fa32b560dcad733c26d71b
Lucas Oliveira <lucasolisocialmedia@gmail.com>
2026-08-06 21:34:46 -0300

    1144

---
+++ src/main/java/org/dsa/challanges_beecrowd/E_1144.java
@@ -0,0 +1,18 @@
+package org.dsa.challanges_beecrowd;
+
+import java.util.Scanner;
+
+public class E_1144 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            int quadrado = i * i;
+            int cubo = i * i * i;
+
+            System.out.println(i + " " + quadrado + " " + cubo);
+            System.out.println(i + " " + (quadrado + 1) + " " + (cubo + 1));
+        }
+    }
+}
