47042d1077df2552e7afd919dd2432a40f156139
Lucas Oliveira <lucasolisocialmedia@gmail.com>
2026-08-06 21:34:42 -0300

    1073

---
+++ src/main/java/org/dsa/challanges_beecrowd/E_1073.java
@@ -0,0 +1,17 @@
+package org.dsa.challanges_beecrowd;
+
+import java.util.Scanner;
+
+public class E_1073 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                System.out.printf("%d ^ 2 = %d\n", i, (i * i));
+            }
+        }
+    }
+}
