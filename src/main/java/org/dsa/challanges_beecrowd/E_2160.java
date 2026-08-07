bbd220eae5f12d18992211b1ee260ae675eb73f2
Lucas Oliveira <lucasolisocialmedia@gmail.com>
2026-08-06 21:34:49 -0300

    2160

---
+++ src/main/java/org/dsa/challanges_beecrowd/E_2160.java
@@ -0,0 +1,17 @@
+package org.dsa.challanges_beecrowd;
+
+import java.util.Scanner;
+
+public class E_2160 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        if (s.length() <= 80){
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
