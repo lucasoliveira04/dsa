6e71e47bf3331f5085301bf4c1680f4f9eb19d2b
Lucas Oliveira <lucasolisocialmedia@gmail.com>
2026-08-06 21:38:13 -0300

    1150

---
+++ src/main/python/challanges_beecrowd/1150.py
@@ -0,0 +1,19 @@
+x = int(input(""))
+
+cont = 0
+number_connt = 0
+
+while True:
+    z = int(input(""))
+
+    if z > x:
+        break
+
+while True:
+    number_connt+=x+cont
+    cont+=1
+    
+    if number_connt > z:
+        break
+
+print(cont)
