e9ae5879b03f885fee223852aa017dda2e41405d
Lucas Oliveira <lucasolisocialmedia@gmail.com>
2026-08-06 21:34:44 -0300

    1095

---
+++ src/main/java/org/dsa/challanges_beecrowd/E_1095.java
@@ -0,0 +1,13 @@
+package org.dsa.challanges_beecrowd;
+
+public class E_1095 {
+    public static void main(String[] args) {
+        int i = 1, J = 60;
+
+        while (J >= 0){
+            System.out.println("I=" + i + " J=" + J);
+            i += 3;
+            J -= 5;
+        }
+    }
+}
